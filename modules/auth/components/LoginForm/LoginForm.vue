f87fb066d4652bf0a7543c21d816e0e527d626d9
Alexandre Bisognin <alexandrefb@planmkt.com.br>
2024-09-17 00:58:11 -0300

    initial commit

---
+++ modules/auth/components/LoginForm/LoginForm.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+const form = ref<LoginForm>({
+  email: "",
+  password: "",
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <div class="mb-5">
+      <el-input
+        v-model="form.email"
+        type="email"
+        placeholder="Email"
+        style="margin-bottom: 16px"
+      />
+      <el-input
+        v-model="form.password"
+        type="password"
+        placeholder="Password"
+      />
+    </div>
+
+    <el-button type="primary">Entrar</el-button>
+  </div>
+</template>
---
+++ modules/auth/screens/login/Login.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import LoginForm from '@/modules/auth/components/LoginForm/LoginForm.vue';
+
+</script>
+<template>
+  <div
+    class="container mx-auto flex flex-col items-center justify-center h-screen py-9"
+  >
+    <div
+      class="flex flex-col gap-5 justify-evenly items-center shadow-md w-full max-w-80 min-h-96 px-3 py-5"
+    >
+      <h1 class="text-3xl">Login</h1>
+    <LoginForm />
+    </div>
+  </div>
+</template>
